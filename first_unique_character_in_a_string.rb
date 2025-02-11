13afebf0d13231f47a44fad90d6037624ca873be
CameronBibb <cameron.bibb@icloud.com>
2025-02-11 17:31:32 -0500

    feat(first_unique_char): implement hash-based solution for O(n) efficiency

---
+++ first_unique_character_in_a_string.rb
@@ -0,0 +1,23 @@
+# Given a string, return the first non-repeating character.
+
+def first_unique_char(str)
+  occurrences = Hash.new(0)
+
+  str.each_char { |char| occurrences[char] += 1 }
+
+  str.each_char { |char| return char if occurrences[char] == 1 }
+
+  ""
+end
+
+p first_unique_char("swiss")  # => "w"
+p first_unique_char("racecar")  # => "e"
+
+=begin
+iterate through the string
+  count the number of occurrences of each letter
+    (use a hash to store the number of occurences of a letter)
+iterate through the string again
+  check the number of occurences of each letter
+  the first letter with the value of 1 gets returned
+=end
