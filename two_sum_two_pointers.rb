eef204d908f72d013b7ce5a24adccc4aa5119943
CameronBibb <cameron.bibb@icloud.com>
2025-02-11 09:05:06 -0500

    refactor(two_sum_two_pointers): simplify logic and return early

--- two_sum_two_pointers.rb
+++ two_sum_two_pointers.rb
@@ -5,23 +5,18 @@ def two_sum(arr, target)
   # Your code here (Use Two Pointers)
   a = 0
   b = arr.length - 1
-  result = []
   while a < b
     sum = arr[a] + arr[b]
-    if sum == target
-      result = [a, b]
-      break
-    elsif sum > target
-      b -= 1
-    else
-      a += 1
-    end
+    return [a, b] if sum == target
+    sum > target ? b -= 1 : a += 1
   end
-  result
+
+  []
 end
 
 p two_sum([1, 3, 4, 6, 8, 9], 10) == [0, 5]
 p two_sum([2, 7, 11, 15], 9) == [0, 1]
+p two_sum([1, 2, 3, 4, 5], 10) == []  # Edge case: no solution
 
 =begin
 the array is sorted
