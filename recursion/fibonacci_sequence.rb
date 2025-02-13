5bcb31e87e007f981aafb491384f604da3fbb76d
CameronBibb <cameron.bibb@icloud.com>
2025-02-12 23:40:39 -0500

    add files for recursion problems

---
+++ recursion/fibonacci_sequence.rb
@@ -0,0 +1,8 @@
+# Write a function that returns the nth Fibonacci number using recursion.
+
+def fibonacci(n)
+  # Your code here (Recursive approach)
+end
+
+fibonacci(5)  # => 5
+fibonacci(10)  # => 55
